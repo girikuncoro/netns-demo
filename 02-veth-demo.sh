4ada5add944173e3e232cfb904b5ad62cd662e67
Giri Kuncoro <girikuncoro@gmail.com>
2018-04-13 19:28:49 +0700

    Add better explanation for veth pair demo

--- 02-veth-demo.sh
+++ 02-veth-demo.sh
@@ -1,72 +1,89 @@
 #!/bin/bash
 # this script must be run as root
 
-# create ovs bridge in root namespace
+# create ovs bridge in root namespace. this will be used to connect red and blue namespaces.
 ovs-vsctl add-br ovs1
 
-# create veth pairs in root namespace,
-# move to connect red namespace
-# and ovs bridge
+# create veth pairs in root namespace, move to connect red namespace and ovs bridge. veth pair
+# is like a pipe with 2 network interfaces, such that packet from one end will flow to another
+# end.
 ip link add eth0-r type veth peer name veth-r
+
+# move eth0-r interface to red namespace.
 ip link set eth0-r netns red
 ip netns exec red ip link list
+
+# connect veth-r to ovs bridge. with this, ovs bridge and red namespace are connected, but
+# both interfaces are still down.
 ovs-vsctl add-port ovs1 veth-r
 ovs-vsctl show
 
-# create another pair to connect blue namespace
-# and ovs bridge
+# create another pair to connect blue namespace and ovs bridge.
 ip link add eth0-b type veth peer name veth-b
+
+# move eth0-b interface to blue namespace.
 ip link set eth0-b netns blue
 ip netns exec blue ip link list
+
+# connect veth-b to ovs bridge. with this, ovs bridge and blue namespace are connected, but
+# both interfaces are still down.
 ovs-vsctl add-port ovs1 veth-b
 ovs-vsctl show
 
-# veth-r and veth-b are in root namespace
+# bring up veth-r and veth-b in root namespace.
 ip link list
 ip link set dev veth-r up
 ip link set dev veth-b up
 
-# bring loopback in red namespace
+# bring up loopback in red namespace. verify that localhost is not pinging in red namespace
+# since loopback interface is down, and pinging once we bring it up.
 ip netns exec red ping localhost
 ip netns exec red ip link set dev lo up
 ip netns exec red ping localhost
 
-# set static ip address in red namespace
+# set static ip address in red namespace.
 ip netns exec red ip link set dev eth0-r up
 ip netns exec red ip address add 10.0.0.1/24 dev eth0-r
 
-# check red namespace has route via eth0-r
+# check red namespace has route via eth0-r.
 ip netns exec red ip route
 
-# root namespace doesnt aware of 10.0.0.0/24 network
+# root namespace doesnt aware of 10.0.0.0/24 network.
 ip route
 
-# bring up same thing in blue namespace
+# bring up same thing in blue namespace, assign with different static ip
+# same subnet.
 ip netns exec blue ip link set dev lo up
 ip netns exec blue ip link set dev eth0-b up
 ip netns exec blue ip address add 10.0.0.2/24 dev eth0-b
 
-# check blue namespace has route via eth0-b
+# check blue namespace has route via eth0-b.
 ip netns exec blue ip route
 
-# test pinging blue from red namespace
+# test pinging blue from red namespace, this proof connectivity between
+# red and blue namespaces via ovs bridge.
 ip netns exec red ping 10.0.0.2 -c 2
 
-# test pinging red from blue namespace
+# test pinging red from blue namespace.
 ip netns exec blue ping 10.0.0.1 -c 2
 
-# run netcat server in blue namespace
+# to make it more interesting, run netcat server in blue namespace, listen to
+# port 4000.
 ip netns exec blue nc -l 4000
 
-# accessible from red namespace
+# try running netcat client from blue namespace and send message. this proves
+# loopback interface works in blue namespace.
 ip netns exec blue nc 10.0.0.2 4000
 
-# accessible from blue namespace (loopback)
+# try running netcat client from red namespace and send message. this proves
+# connectivity between red and blue namespaces.
 ip netns exec red nc 10.0.0.2 4000
 
-# not accessible from root namespace
+# try running netcat client on root namespace. netcat server is not accessible 
+# from root namespace, which proves network isolation works.
 nc 10.0.0.2 4000
 
-# find nc PID
+# we can find out a process is owned by which network namespace. find the nc PID,
+# and see that nc process is owned by blue namespace.
 pid=$(ps -ef | grep -m1 nc | cut -d ' ' -f 7)
 ip netns identify ${pid}
